aea20d80fc2b9157d24f69b3ea9de96316c7f3eb
tensfeldt thomas g <thomas.g.tensfeldt@pfizer.com>
2021-04-16 11:22:46 -0400

    Update installation scripts in the install/ directory

--- demo/install/openNCA_install_package.R
+++ demo/install/openNCA_install_package.R
@@ -1,10 +1,6 @@
 ### Installation of openNCA v3.0 specific commit c3d3f48 from demo.zip
 ###  This is the installation procedure to install from packaged demo.zip
-### 2021-03-26/TGT/ First Version
-
-### Installation Tools Required
-library(devtools)
-library(curl)
+### 2021-04-16/TGT/ First Version
 
 ### Remove any prior installation
 if(interactive()) { 
@@ -14,16 +10,9 @@ if(interactive()) {
     }
 }
 
-### commit_zip
-zipfile <- "demo.zip"
-### Download https://github.com/tensfeldt/openNCA/raw/master/demo/demo.zip
-curl_download("https://github.com/tensfeldt/openNCA/raw/master/demo/demo.zip", destfile=zipfile)
-### unzip the install file  
-unzip(zipfile, exdir=getwd())
-list.files()
-### find unzipped directory
-install_taz <- list.files(path=file.path(getwd(), "demo"), pattern="*.tar.gz")
+### find package file in the *release\under{}files* directory
+install_taz <- list.files(path=file.path(getwd(), "release_files"), pattern="*.tar.gz")
 ### Install Package  
-install.packages(file.path(getwd(), "demo", install_taz), repos = NULL, type="source")
+install.packages(file.path(getwd(), "release_files", install_taz), repos = NULL, type="source")
 ### Load Package
 library(openNCA)
